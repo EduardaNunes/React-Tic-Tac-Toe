02f5c892a8f2dc122ab459f22af82ae630783f8d
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2024-12-23 05:16:38 -0300

    Changes remote origin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ufjf-dcc207-2024-3-a-trb1-EduardaNunes",
+  base: "/React-Tic-Tac-Toe",
   plugins: [react()],
 })
